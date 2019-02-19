9fb5904126a234e117fa1565e849f11dce097e7c
Dominic Hulsey <dominic.hulsey.dh@gmail.com>
2019-02-19 11:43:57 -0700

    styling updates

--- app/components/models/sw.js
+++ app/components/models/sw.js
@@ -19,7 +19,7 @@ export default class People {
   getTemplate() {
     return `<div class="col-3 my-1">
     <div class="card bg-light">
-<h5 class="card-title"><u>${this.name}</u></h5>
+<h5 class="card-title text-center"><u>${this.name}</u></h5>
 <h5 class="card-subtitle py-2">Height:<span class="text-primary"> ${this.height}cm.</span></h5>
 <h5 class="card-subtitle py-2">weight:<span class="text-primary"> ${this.mass}kgs.</span></h5>
 <h5 class="card-text py-2">BMI:<span class="text-success">${this.bmi}</span></h5>
